18d9a5c0f2d11378b655815700ac0066dcf46d00
Gang Wang <wanggang1987@live.cn>
2016-10-20 18:31:50 +0800

    database update

---
+++ src/main/java/com/whoknows/picture/Collector.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.whoknows.picture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Collector {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+}
---
+++ src/main/java/com/whoknows/picture/Service.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.whoknows.picture;
+
+import java.io.InputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Service {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    public Long putPicture(InputStream in) {
+        
+        return null;
+    }
+
+    public Long getPicture() {
+        return null;
+    }
+
+}
