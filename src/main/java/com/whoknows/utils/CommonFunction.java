9ed5f164e0dd1fb72f926e6bd89c01247a4d8c95
Wang Gang <wanggang1987@live.cn>
2016-11-12 15:36:40 +0800

    fix username bug

--- src/main/java/com/whoknows/utils/CommonFunction.java
+++ src/main/java/com/whoknows/utils/CommonFunction.java
@@ -8,13 +8,13 @@ public class CommonFunction {
 
 	public static String getUserName(String first_name, String last_name, String email) {
 		String name = new String();
-		if (last_name != null && StringUtils.isNotEmpty(last_name)) {
+		if (StringUtils.isNotEmpty(last_name)) {
 			name += last_name;
 		}
-		if (first_name != null && StringUtils.isNotEmpty(first_name)) {
+		if (StringUtils.isNotEmpty(first_name)) {
 			name += first_name;
 		}
-		if (StringUtils.isNotEmpty(email)) {
+		if (StringUtils.isEmpty(name)) {
 			name = email;
 		}
 		return name;
