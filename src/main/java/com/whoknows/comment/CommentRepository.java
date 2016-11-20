c4a303c0f6cadfd82073ef97ad10e145cbe3bd8e
Wang Gang <wanggang1987@live.cn>
2016-11-20 13:59:48 +0800

    fix comment rank issue

--- src/main/java/com/whoknows/comment/CommentRepository.java
+++ src/main/java/com/whoknows/comment/CommentRepository.java
@@ -14,7 +14,7 @@ public class CommentRepository {
 
 	public List<Long> getReplyComments(Long replyId, int page, int pageSize) {
 		return jdbcTemplate.query("select id from comment where reply_id = ? "
-				+ "order by rank desc "
+				+ "order by id desc "
 				+ "limit ? OFFSET ? ",
 				ps -> {
 					ps.setLong(1, replyId);
