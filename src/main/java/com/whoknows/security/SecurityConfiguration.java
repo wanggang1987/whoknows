f05941a3886d55348f0f35a8b1a89506357565aa
Wang Gang <wanggang1987@live.cn>
2016-11-20 14:04:11 +0800

    fix issue

--- src/main/java/com/whoknows/security/SecurityConfiguration.java
+++ src/main/java/com/whoknows/security/SecurityConfiguration.java
@@ -52,7 +52,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 				.antMatchers("/token/**").permitAll()
 				.antMatchers(HttpMethod.GET, "/img/**").permitAll()
 				// other spring mvn controller need auth
-				.antMatchers("/search/**").permitAll()
+				.antMatchers("/search/**").authenticated()
 				.antMatchers("/tag/**").authenticated()
 				.antMatchers("/topic/**").authenticated()
 				.antMatchers(HttpMethod.POST, "/img/**").authenticated()
