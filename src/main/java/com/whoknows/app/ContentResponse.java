7b2a1193028035becb9203ae338bba36c3682ea1
moheqionglin <zhouwanli2009@yeah.net>
2016-11-24 21:36:08 +0800

    add config

---
+++ src/main/java/com/whoknows/app/ContentResponse.java
@@ -0,0 +1,18 @@
+package com.whoknows.app;
+
+public class ContentResponse {
+
+
+	private String content;
+	public ContentResponse(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
